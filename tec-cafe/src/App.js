4f40445b2d47729aeec05f6957a3dfed68800a04
Evan Zhou <evanzhou413@gmail.com>
2023-11-19 12:49:16 -0800

    add react app

---
+++ tec-cafe/src/App.js
@@ -0,0 +1,28 @@
+
+import * as React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Header from "./components/Header";
+
+import './App.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
